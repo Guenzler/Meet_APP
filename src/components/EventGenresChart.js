71169f28b9f6a86b13d5a08911ca06e40c4109b7
Guenzler <148702094+Guenzler@users.noreply.github.com>
2024-06-19 10:51:00 +0200

    adjust display of eventGenresChart for smaller screens

--- src/components/EventGenresChart.js
+++ src/components/EventGenresChart.js
@@ -9,6 +9,9 @@ const EventGenresChart = ({ events }) => {
     const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'Angular'];
     const colors = ['#0099cc', '#4d4dff', '#002db3', '#00cc99', '#9933ff'];
 
+    const isSmallScreen = window.innerWidth < 600; // can be adjusted based on desired breakpoint
+    const outerRadius = isSmallScreen ? 80 : 130; // Adjust the outer radius for small screens
+
     useEffect(() => {
         setData(getData());
     }, [JSON.stringify(events)]);
@@ -36,6 +39,7 @@ const EventGenresChart = ({ events }) => {
                 fill="#000000"
                 textAnchor={x > cx ? 'start' : 'end'}
                 dominantBaseline="central"
+                style={{ fontSize: '13px', whiteSpace: 'pre-wrap' }} // Adjust font size of label
             >
                 {`${genres[index]} ${(percent * 100).toFixed(0)}%`}
             </text>
@@ -43,12 +47,12 @@ const EventGenresChart = ({ events }) => {
     };
 
     return (
-        <ResponsiveContainer width="99%" height={400}>
+        <ResponsiveContainer width="99%" height={isSmallScreen ? 300 : 400}>
             <PieChart margin={{
                     top: 0,
-                    right: 20,
-                    bottom: 0,
-                    left: 20,
+                    right: 40,
+                    bottom: 15,
+                    left: 70,
                 }}>
                 <Pie
                     data={data}
@@ -56,7 +60,7 @@ const EventGenresChart = ({ events }) => {
                     fill="#000000"
                     labelLine={false}
                     label={renderCustomizedLabel}
-                    outerRadius={130}
+                    outerRadius={outerRadius}
                 >
                     {
                         data.map((entry, index) => (
@@ -64,7 +68,7 @@ const EventGenresChart = ({ events }) => {
                         ))
                     }
                 </Pie>
-                <Legend verticalAlign="bottom" align="center" height={36}/>
+                <Legend verticalAlign="bottom" align="center" height={42}/>
             </PieChart>
         </ResponsiveContainer>
     );
